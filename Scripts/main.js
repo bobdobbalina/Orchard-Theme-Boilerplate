3e11cd01f203d1a7f99233a61657e0b10e799149
Mike Grunwald <mike@northernground.com>
2014-07-18 13:10:20 -0500

    Updating the initial namespace declaration.

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,6 +4,6 @@
   Client NAMESPACE
 ************************************/
 
-Client = {};
+window.Client = ( typeof window.Client == "undefined" ) ? {} : window.Client;
 
 //@prepros-append NorthernGround/Client.Main.js
